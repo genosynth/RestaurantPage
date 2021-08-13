21ffc3e93b17b775700659bddd065de8c80aff49
Jean <jpgenovese@gmail.com>
2021-08-13 14:55:53 +0200

    Almost ready

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import { title, describtion1, describtion2, describtion3, image } from "./home"
 import { titleAbout, viewAddress, imageAbout, contact } from "./about"
-import {titleMenu} from "./menu"
+import {titleMenu, imagesMenu, titlesMenu, describtionsMenu} from "./menu"
 
 /*LOADS THE HOME TAB INFO */
 
@@ -44,6 +44,25 @@ document.getElementById("menu").addEventListener("click", function() {
   document.getElementById("about").classList.remove("active");
   document.getElementById("menu").className = "active";
   document.getElementById("content").appendChild(titleMenu());
+  let menu = document.createElement("div");
+  menu.className="menu";
+  let array = imagesMenu(); //This gets the array of img urls imported from menu.js
+  let arrayTitles = titlesMenu(); //This gets the array of header 2 titles imported from menu.js 
+  let arrayDescribtions = describtionsMenu(); ////This gets the array of describtion paragraphs imported from menu.js 
 
+
+  for (let i=0; i<array.length; i++){
+    let div = document.createElement("div");
+    div.className ="menu-item";  
+    
+    div.appendChild(arrayTitles[i]);
+    div.appendChild(array[i]);
+    div.appendChild(arrayDescribtions[i]);
+    menu.appendChild(div);
+  }
+
+  document.getElementById("content").appendChild(menu);
+
+  
 
 })
--- src/menu.js
+++ src/menu.js
@@ -6,4 +6,62 @@ function titleMenu() {
     return element;
   }
 
-  export {titleMenu}
+let array = [
+  {name:"Angus Beef", imgurl:"./images/angus.jpg", describtion:"A popular breed of black cattle that ranges in quality, but can yield welbeel-marbled f."},
+  {name:"Bone Roast Stake", imgurl:"./images/boneroast.jpg", describtion:"A large, flavourful chuck roast with a 7-shaped bone best cooked slowly on long heat."},
+  {name:"Chuck Stake", imgurl:"./images/chuck.jpg", describtion:"Chewy, flavorful, budget-friendly shoulder steaks best marinated and grilled or braised."},
+  {name:"Ostrich", imgurl:"./images/ostrich.jpg", describtion:"A quick-cooking tender meat more similar in flavor and texture to lean beef than poultry."},
+  {name:"Beef Sirloin", imgurl:"./images/sirloin.jpg", describtion:"A portion of the beef loin primal cut that yields lean meat ideal for grinding or braisin."},
+  {name:"Beef Tenderloin", imgurl:"./images/tenderloin.jpg", describtion:"A tender, lean and highly prized part of the loin made into steaks and roasts."},
+
+
+
+]
+
+  function imagesMenu() { //This function will return an array with all the elements of the images
+
+    let imagesElements = [];
+
+    for (let i=0; i<array.length; i++){
+      const image = document.createElement('img');
+      image.className = "menu-images";
+      image.src = array[i].imgurl;
+
+      imagesElements.push(image);
+    }
+
+
+    return imagesElements;
+
+  }
+
+  function titlesMenu() {
+
+    let titlesElements = [];
+
+    for (let i=0; i<array.length; i++){
+      const element = document.createElement('h2');
+      element.innerText = array[i].name;
+
+      titlesElements.push(element);
+    }
+
+    return titlesElements;
+
+  }
+
+  function describtionsMenu() {
+
+    let describtionElements = [];
+
+    for (let i=0; i<array.length; i++){
+      const element = document.createElement('p');
+      element.innerText = array[i].describtion;
+
+      describtionElements.push(element);
+    }
+
+    return describtionElements;
+  }
+
+  export {titleMenu, imagesMenu, titlesMenu, describtionsMenu }
